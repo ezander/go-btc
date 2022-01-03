b251cc2a7d84ecda5cedb20bc046435e6d5ee85a
Elmar Zander <elmar@zandere.de>
2022-01-03 22:30:03 +0100

    Export fields from version message

--- network/messages.go
+++ network/messages.go
@@ -24,17 +24,17 @@ func unmarshalMessage(command string, data []byte) (Message, []byte) {
 }
 
 type VersionMessage struct {
-	version   uint32    // Identifies protocol version being used by the node
-	services  uint64    // bitfield of features to be enabled for this connection
-	timestamp time.Time // standard UNIX timestamp in seconds
-	addr_recv NetAddr   // The network address of the node receiving this message
-	//Fields below require version ≥ 106
-	addr_from    NetAddr // Field can be ignored. This used to be the network address of the node emitting this message, but most P2P implementations send 26 dummy bytes. The "services" field of the address would also be redundant with the second field of the version message.
-	nonce        uint64  // 	Node random nonce, randomly generated every time a version packet is sent. This nonce is used to detect connections to self.
-	user_agent   string  //	User Agent (0x00 if string is 0 bytes long)
-	start_height uint32  // The last block received by the emitting node
+	Version      uint32    // Identifies protocol version being used by the node
+	Services     uint64    // bitfield of features to be enabled for this connection
+	Timestamp    time.Time // standard UNIX timestamp in seconds
+	ReceiverAddr NetAddr   // The network address of the node receiving this message
+	// Fields below require version ≥ 106
+	FromAddr    NetAddr // Field can be ignored. This used to be the network address of the node emitting this message, but most P2P implementations send 26 dummy bytes. The "services" field of the address would also be redundant with the second field of the version message.
+	Nonce       uint64  // 	Node random nonce, randomly generated every time a version packet is sent. This nonce is used to detect connections to self.
+	UserAgent   string  //	User Agent (0x00 if string is 0 bytes long)
+	StartHeight uint32  // The last block received by the emitting node
 	// Fields below require version ≥ 70001
-	relay bool // 	Whether the remote peer should announce relayed transactions or not, see BIP 0037
+	Relay bool // 	Whether the remote peer should announce relayed transactions or not, see BIP 0037
 }
 
 // Bitfield constants for services
@@ -50,49 +50,49 @@ const NODE_NETWORK_LIMITED = 1024 // 	See BIP 0159
 func NewVersionMessage() VersionMessage {
 
 	msg := VersionMessage{
-		version:      31800,
-		services:     NODE_NETWORK,
-		timestamp:    time.Now().Truncate(time.Second),
-		addr_recv:    NetAddr{NODE_NETWORK, net.IPv4(127, 0, 0, 1), 8333},
-		addr_from:    NetAddr{NODE_NETWORK, net.IPv4(127, 0, 0, 1), 8333},
-		nonce:        3141526,
-		user_agent:   "Foobar client v0.1",
-		start_height: 1,
-		relay:        false,
+		Version:      31800,
+		Services:     NODE_NETWORK,
+		Timestamp:    time.Now().Truncate(time.Second),
+		ReceiverAddr: NetAddr{NODE_NETWORK, net.IPv4(127, 0, 0, 1), 8333},
+		FromAddr:     NetAddr{NODE_NETWORK, net.IPv4(127, 0, 0, 1), 8333},
+		Nonce:        3141526,
+		UserAgent:    "Foobar client v0.1",
+		StartHeight:  1,
+		Relay:        false,
 	}
 	return msg
 }
 
 func (v VersionMessage) Marshal(out []byte) []byte {
-	out = marshalUint32(out, v.version)
-	out = marshalUint64(out, v.services)
-	out = MarshalTimestamp(out, v.timestamp)
-	out = MarshalNetAddr(out, v.addr_recv)
-	if v.version >= 106 {
-		out = MarshalNetAddr(out, v.addr_from)
-		out = MarshalNonce(out, v.nonce)
-		out = MarshalVarStr(out, v.user_agent)
-		out = marshalUint32(out, v.start_height)
+	out = marshalUint32(out, v.Version)
+	out = marshalUint64(out, v.Services)
+	out = MarshalTimestamp(out, v.Timestamp)
+	out = MarshalNetAddr(out, v.ReceiverAddr)
+	if v.Version >= 106 {
+		out = MarshalNetAddr(out, v.FromAddr)
+		out = MarshalNonce(out, v.Nonce)
+		out = MarshalVarStr(out, v.UserAgent)
+		out = marshalUint32(out, v.StartHeight)
 	}
-	if v.version >= 70001 {
-		out = MarshalBool(out, v.relay)
+	if v.Version >= 70001 {
+		out = MarshalBool(out, v.Relay)
 	}
 	return out
 }
 
 func (msg *VersionMessage) Unmarshal(out []byte) []byte {
-	msg.version, out = unmarshalUint32(out)
-	msg.services, out = unmarshalUint64(out)
-	msg.timestamp, out = UnmarshalTimestamp(out)
-	msg.addr_recv, out = UnmarshalNetAddr(out)
-	if msg.version >= 106 {
-		msg.addr_from, out = UnmarshalNetAddr(out)
-		msg.nonce, out = UnmarshalNonce(out)
-		msg.user_agent, out = UnmarshalVarStr(out)
-		msg.start_height, out = unmarshalUint32(out)
+	msg.Version, out = unmarshalUint32(out)
+	msg.Services, out = unmarshalUint64(out)
+	msg.Timestamp, out = UnmarshalTimestamp(out)
+	msg.ReceiverAddr, out = UnmarshalNetAddr(out)
+	if msg.Version >= 106 {
+		msg.FromAddr, out = UnmarshalNetAddr(out)
+		msg.Nonce, out = UnmarshalNonce(out)
+		msg.UserAgent, out = UnmarshalVarStr(out)
+		msg.StartHeight, out = unmarshalUint32(out)
 	}
-	if msg.version >= 70001 {
-		msg.relay, out = UnmarshalBool(out)
+	if msg.Version >= 70001 {
+		msg.Relay, out = UnmarshalBool(out)
 	}
 	return out
 }
